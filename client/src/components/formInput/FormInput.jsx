687efdc1a82d9aff74f6e22f9e4b20727d659a68
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-02 03:25:21 +0000

    created my Form Input Elements

---
+++ client/src/components/formInput/FormInput.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const FormInput = (props) => {
+  return (
+    <div className="flex flex-col gap-2.5">
+      {/* <label htmlFor={props.LabelForName}>{props.Label}</label> */}
+      <input
+        type={props.inputType}
+        placeholder={props.inputPlaceholder}
+        className="outline-none border rounded-lg p-1"
+        name={props.inputName}
+      />
+     
+    </div>
+  );
+}
+
+export default FormInput
