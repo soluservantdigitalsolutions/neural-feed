0ed0fae2208fa031a0281f6ed45e82503974a605
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-02 03:25:38 +0000

    Created my Logo omponent

---
+++ client/src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">
+        <span className="text-green-600">Neural</span> Feed
+      </h1>
+    </div>
+  );
+};
+
+export default Logo;
