442d735f1fe578761f886b2e1ec04cdc8dda7bfd
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-04 04:34:39 +0000

    created a secondary button Component

---
+++ client/src/components/SecondaryButton/SecondaryButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SecondaryButton = (props) => {
+  return (
+    <div className="LoginButtonDiv border rounded border-green-600 flex justify-center items-center font-bold">
+      <button className="text-green-600 text-lg p-1">{props.SecondaryButtonText}</button>
+    </div>
+  );
+}
+
+export default SecondaryButton
