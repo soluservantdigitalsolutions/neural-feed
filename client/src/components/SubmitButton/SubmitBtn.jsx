5f59fc1377953cfa602296fc82fb9aaf97526ed7
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-04 04:52:54 +0000

    restored to default

--- client/src/components/SubmitButton/SubmitBtn.jsx
+++ client/src/components/SubmitButton/SubmitBtn.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SubmitBtn = (props) => {
   return (
     <div className="border rounded bg-green-600 flex justify-center items-center font-bold">
-      <button className="text-white text-lg p-2">
-        {props.ButtonText}
-      </button>
+      <button className="text-white text-lg p-1">{props.ButtonText}</button>
     </div>
   );
 };
-
 
 export default SubmitBtn;
