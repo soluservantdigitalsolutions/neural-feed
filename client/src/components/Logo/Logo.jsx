a0948507c1e757ed560e20c49c1807677d308641
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-04 04:31:29 +0000

    added a link with an href to home to the logo Component

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const Logo = () => {
   return (
     <div>
-      <h1 className="font-bold text-2xl">
-        <span className="text-green-600">Neural</span> Feed
-      </h1>
+      <a href="/">
+        <h1 className="font-bold text-2xl cursor-pointer ">
+          <span className="text-green-600">Neural</span> Feed
+        </h1>
+      </a>
     </div>
   );
 };
