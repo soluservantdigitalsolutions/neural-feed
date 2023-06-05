c7480c18fd2fe24c863674ee33cb9e1ab4a7a6ff
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-05 05:07:37 +0000

    added hover styling classes

--- client/src/components/SecondaryButton/SecondaryButton.jsx
+++ client/src/components/SecondaryButton/SecondaryButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SecondaryButton = (props) => {
   return (
     <div className="LoginButtonDiv border rounded border-green-600 flex justify-center items-center font-bold">
-      <button className="text-green-600 text-lg p-1">{props.SecondaryButtonText}</button>
+      <button className="text-green-600 text-lg p-1 hover:bg-green-600 transition hover:text-white">{props.SecondaryButtonText}</button>
     </div>
   );
 }
