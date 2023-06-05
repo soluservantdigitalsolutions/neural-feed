68ca7250cafe0bc2ce5cf55bc440b61c721e27b4
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-05 05:11:28 +0000

    created a minimum viable product upload page

--- client/src/pages/UploadPage/Upload.jsx
+++ client/src/pages/UploadPage/Upload.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
 import Dropzone from '../../components/DropZone/Dropzone';
+import FormInput from '../../components/formInput/FormInput';
+import SubmitBtn from '../../components/SubmitButton/SubmitBtn';
+import SecondaryButton from '../../components/SecondaryButton/SecondaryButton';
 
 const Upload = () => {
   return (
     <div
-      className="MainFormCard border p-5 rounded flex justify-center  flex-col gap-2.5 shadow-xl "
-      style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-      }}
+      className="  MainFormCard border p-5 rounded flex justify-center  flex-col gap-2.5 shadow-xl m-5 "
+      style={
+        {
+          // position: 'absolute',
+          // top: '50%',
+          // left: '50%',
+          // transform: 'translate(-50%, -50%)',
+        }
+      }
     >
-      <Dropzone/>
+      <div className="UploadTitle ">
+        <h1 className="text-xl font-bold">Upload Your Neural Feed</h1>
+      </div>
+      <div className="UploadVideoDiv flex gap-5">
+        <div className="VideoDropZoneDiv">
+          <Dropzone />
+        </div>
+        <div className="VideoUploadDetailsDiv w-full flex flex-col gap-5">
+          <div className="captionDiv">
+            <h1 className="Caption font-semibold">Caption</h1>
+            <input
+              type="text"
+              name="caption"
+              id=""
+              className="outline-none border rounded p-3 w-full"
+            />
+          </div>
+          <div className="ActionButtonsDiv flex gap-2 ">
+            {/* <div className="border rounded border-green-600 flex justify-center items-center font-semibold">
+              <button className="text-green-600 text-lg p-1">Discard</button>
+            </div> */}
+            <SecondaryButton SecondaryButtonText="Discard"/>
+            <SubmitBtn ButtonText="Feed" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
