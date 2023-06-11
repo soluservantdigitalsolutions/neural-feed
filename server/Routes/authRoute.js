9790a3b34f708894f55fa101a84e05f69f0e3899
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-11 19:12:11 +0000

    created login route

--- server/Routes/authRoute.js
+++ server/Routes/authRoute.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const { Register } = require("../Controllers/AuthController.js");
+const { Register, Login } = require("../Controllers/AuthController.js");
 const User = require("../Models/UserModel.js");
 
+//Implementing auth Routes
+
+
 router.post("/register", Register);
+
+router.post("/login", Login)
 
 module.exports = router;
