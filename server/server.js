55053ba886b8e99f468c54fa1d2b9152f39399cd
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-11 18:43:25 +0000

    setup my server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoute = require("./Routes/authRoute.js");
+const cookieParser = require("cookie-parser");
 dotenv.config();
+
+//middlewares
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
 
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
-    console.log('Database is connected');
+    console.log("Database is connected");
   })
   .catch((err) => {
     console.log(err);
