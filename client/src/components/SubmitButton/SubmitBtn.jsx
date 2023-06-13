252653c356184108fed3351128f83caf9407f925
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-13 18:51:41 +0000

    added a few style changes to submit button component

--- client/src/components/SubmitButton/SubmitBtn.jsx
+++ client/src/components/SubmitButton/SubmitBtn.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const SubmitBtn = (props) => {
   return (
-    <div className="border rounded bg-green-600 flex justify-center items-center font-bold">
-      <button className="text-white text-lg p-1">{props.ButtonText}</button>
+    <div className="border rounded transition  bg-black flex justify-center items-center font-bold hover:shadow-2xl hover:bg-green-600">
+      <button className="text-white text-lg p-1 ">{props.ButtonText}</button>
     </div>
   );
 };
