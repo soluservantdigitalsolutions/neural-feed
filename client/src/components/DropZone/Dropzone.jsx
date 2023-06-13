54444e107307111918a730ba3ce1aac0adc3fc71
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-13 18:49:05 +0000

    made a few style changes to input element file

--- client/src/components/DropZone/Dropzone.jsx
+++ client/src/components/DropZone/Dropzone.jsx
@@ -2,32 +2,31 @@ import React from 'react';
 import { RiVideoUploadFill } from 'react-icons/ri';
 import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import "./DropZone.css"
 
 const Dropzone = () => {
   
   return (
     <div className="UploadCardSpaceDiv border-dashed border-green-600 bg-slate-100 cursor-pointer hover:bg-green-100 transition flex flex-col justify-center text-center p-5 border">
       <div className="UploadIconDiv flex flex-col justify-center items-center text-center">
-        <RiVideoUploadFill className="text-5xl text-center " />
+        <RiVideoUploadFill  className="text-5xl text-center " />
         <h1>Click to Upload</h1>
       </div>
       {/* <div className="InstructionalDiv">
         <p>Or Drag and Drop your Video File</p>
       </div> */}
-      <div
-        className="UploadButtonDiv flex justify-center items-center"
-      >
+      <div className="UploadButtonDiv flex justify-center items-center">
         <input
           type="file"
           name=""
           id=""
-            className="text-center justify-center items-center"
-            style={{
-              display: 'flex',
-              textAlign: 'center',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
+          className=" inputfile file:bg-green-600 file:rounded file:border-none file:flex file:align-center text-center justify-center items-center"
+          style={{
+            display: "flex",
+            textAlign: "center",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         />
       </div>
     </div>
