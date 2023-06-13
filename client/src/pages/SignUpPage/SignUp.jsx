2e085fd7e9e9d3c1e78e4807c9822d4867b7c142
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-13 18:52:06 +0000

    coded the register or sign up function

--- client/src/pages/SignUpPage/SignUp.jsx
+++ client/src/pages/SignUpPage/SignUp.jsx
@@ -1,37 +1,97 @@
-import React from 'react'
-import Logo from '../../components/Logo/Logo';
-import FormInput from '../../components/formInput/FormInput';
-import SubmitBtn from '../../components/SubmitButton/SubmitBtn';
-import GoogleButton from 'react-google-button';
+import React from "react";
+import Logo from "../../components/Logo/Logo";
+import FormInput from "../../components/formInput/FormInput";
+import SubmitBtn from "../../components/SubmitButton/SubmitBtn";
+import GoogleButton from "react-google-button";
 // import { signInWithGoogle } from '../../GoogleAuthFunc/GoogleAuth';
-import { useNavigate } from 'react-router-dom';
-import { Auth, Provider } from '../../../firebase.config';
-import { signInWithPopup } from 'firebase/auth';
-
+import { useNavigate } from "react-router-dom";
+import { Auth, Provider } from "../../../firebase.config";
+import { signInWithPopup } from "firebase/auth";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 
 const SignUp = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { username, email, password, confirmPassword } = inputValue;
 
-    const signInWithGoogle = () => {
-      signInWithPopup(Auth, Provider)
-        .then(() => {
-          navigate('/profile');
-          console.log('Signed In');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+  const HandleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+    console.log(inputValue);
+  };
+
+  const handleError = (err) => {
+    toast.error(err, {
+      position: "bottom-left",
+    });
+  };
+  const handleSuccess = (msg) => {
+    toast.success(msg, {
+      position: "bottom-right",
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/api/auth/register",
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    setInputValue({
+      ...inputValue,
+      username:"",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
+  const signInWithGoogle = () => {
+    signInWithPopup(Auth, Provider)
+      .then(() => {
+        navigate("/profile");
+        console.log("Signed In");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="main border">
       <div
         className="MainFormCard border p-5 rounded flex justify-center items-center flex-col gap-2.5 shadow-xl"
         style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
         }}
       >
         <div className="LogoDiv ">
@@ -39,15 +99,17 @@ const SignUp = () => {
         </div>
         <div className="FormDiv flex flex-col gap-2">
           <form
+            onSubmit={handleSubmit}
             action=""
             className="flex flex-col gap-2.5 "
           >
             <FormInput
               inputType="name"
-              inputPlaceholder="Name"
-              Label="Email"
-              LabelForName="email"
-              inputName="email"
+              inputPlaceholder="Username"
+              LabelForName="username"
+              inputName="username"
+              value={username}
+              onChange={HandleOnChange}
             />
             <FormInput
               inputType="email"
@@ -55,6 +117,8 @@ const SignUp = () => {
               Label="Email"
               LabelForName="email"
               inputName="email"
+              value={email}
+              onChange={HandleOnChange}
             />
             <FormInput
               inputType="password"
@@ -62,22 +126,26 @@ const SignUp = () => {
               Label="Password"
               LabelForName="password"
               inputName="password"
+              value={password}
+              onChange={HandleOnChange}
             />
             <FormInput
               inputType="password"
               inputPlaceholder="Confirm Password"
               Label="Password"
               LabelForName="password"
-              inputName="password"
+              inputName="confirmPassword"
+              value={confirmPassword}
+              onChange={HandleOnChange}
             />
             <SubmitBtn ButtonText="Join" />
           </form>
           <div className="googleButtonDiv">
-            <GoogleButton onClick={signInWithGoogle}/>
+            <GoogleButton onClick={signInWithGoogle} />
           </div>
           <div className="LoginQuestionDiv">
             <p>
-              Already Have an Account?{' '}
+              Already Have an Account?{" "}
               <a href="/login">
                 <u>login</u>
               </a>
@@ -85,8 +153,9 @@ const SignUp = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
-}
+};
 
-export default SignUp
+export default SignUp;
