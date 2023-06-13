b10f46c21db8cc50c3feb1e3d00b11bb94b7abfd
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-13 17:17:19 +0000

    imported bcrypt and dotenv and wrote the login middleware functionality

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,5 +1,8 @@
 const UserModel = require("../Models/UserModel");
 const createSecretToken = require("../utils/secretToken");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const Register = async (req, res, next) => {
   try {
@@ -10,7 +13,6 @@ const Register = async (req, res, next) => {
     if (existingUser) {
       return res.json({ message: "User Already Exists" });
     }
-    
 
     const newUser = await UserModel.create({
       username,
@@ -32,7 +34,32 @@ const Register = async (req, res, next) => {
   }
 };
 
-const Login = async (req, res) => {};
+const Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(500).json({ message: "All fields are required!" });
+    }
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.json({ message: "Incorrect Email or Password!" });
+    }
+    const userPassword = await bcrypt.compare(password, user.password);
+    if (!userPassword) {
+      return res.json({ message: "Incorrect Password or Email" });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User has been logged in successfully", success: true });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
   Register,
