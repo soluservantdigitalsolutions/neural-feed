e0c83bf3e1d9761ea212a41ede883443bd84b14e
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-16 23:25:02 +0000

    linted my file

--- client/src/components/DropZone/Dropzone.jsx
+++ client/src/components/DropZone/Dropzone.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { RiVideoUploadFill } from 'react-icons/ri';
-import { useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
-import "./DropZone.css"
+import React from "react";
+import { RiVideoUploadFill } from "react-icons/ri";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import "./DropZone.css";
 
 const Dropzone = () => {
-  
   return (
     <div className="UploadCardSpaceDiv border-dashed border-green-600 bg-slate-100 cursor-pointer hover:bg-green-100 transition flex flex-col justify-center text-center p-5 border">
       <div className="UploadIconDiv flex flex-col justify-center items-center text-center">
-        <RiVideoUploadFill  className="text-5xl text-center " />
+        <RiVideoUploadFill className="text-5xl text-center " />
         <h1>Click to Upload</h1>
       </div>
       {/* <div className="InstructionalDiv">
