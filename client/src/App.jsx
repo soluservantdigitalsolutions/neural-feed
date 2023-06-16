7fbcdb2efd12188297c9619d1128d32d8d5fe3e5
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-16 23:24:24 +0000

    created new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react';
-import './App.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Login from './pages/LoginPage/Login';
-import SignUp from './pages/SignUpPage/SignUp';
-import Home from './pages/Home/Home';
-import Profile from './pages/profilePage/Profile';
-import NavBar from './components/NavBar/NavBar';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Auth } from '../firebase.config';
-import Upload from './pages/UploadPage/Upload';
+import { useState } from "react";
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/LoginPage/Login";
+import SignUp from "./pages/SignUpPage/SignUp";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/profilePage/Profile";
+import NavBar from "./components/NavBar/NavBar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Auth } from "../firebase.config";
+import Upload from "./pages/UploadPage/Upload";
 
 function App() {
- 
-
   return (
     <>
       <NavBar />
       <Routes>
         <Route
           index
-          path="/login"
-          element={<Login />}
+          exact
+          path="/"
+          element={<Home />}
         />
         <Route
           path="/register"
           element={<SignUp />}
         />
         <Route
-          path="/"
-          element={<Home />}
+          path="/login"
+          element={<Login />}
         />
         <Route
           path="/profile"
