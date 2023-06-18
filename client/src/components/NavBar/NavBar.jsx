6fb6b8278f72e0ebb4ffdd456840ac8b33e80851
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-18 00:55:07 +0000

    linted my file and imported custom hooks for logout and useAuthContext

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
-import Logo from '../Logo/Logo';
-import FormInput from '../formInput/FormInput';
-import SubmitBtn from '../SubmitButton/SubmitBtn';
-import { FaRegHandPaper } from 'react-icons/fa';
-import SecondaryButton from '../SecondaryButton/SecondaryButton';
-import { Auth } from '../../../firebase.config';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { AiFillNotification } from 'react-icons/ai';
-import DropdownMenu from '../DropdownMenu.jsx/DropdownMenu';
-import { signOut } from 'firebase/auth';
-import { AiOutlineLogout } from 'react-icons/ai';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Logo from "../Logo/Logo";
+import FormInput from "../formInput/FormInput";
+import SubmitBtn from "../SubmitButton/SubmitBtn";
+import { FaRegHandPaper } from "react-icons/fa";
+import SecondaryButton from "../SecondaryButton/SecondaryButton";
+import { Auth } from "../../../firebase.config";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { AiFillNotification } from "react-icons/ai";
+import DropdownMenu from "../DropdownMenu.jsx/DropdownMenu";
+import { signOut } from "firebase/auth";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useNavigate, Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout";
 // import { Logout } from '../../pages/Home/Home';
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const [user] = useAuthState(Auth);
+  // const [user] = useAuthState(Auth);
   const [toggleDropdown, setToggleDropdown] = useState(false);
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const handleLogout = () => {
+    logout();
+  };
 
   return (
     <div className="border flex  justify-between items-center p-3">
@@ -55,9 +63,18 @@ const NavBar = () => {
               className="w-10 rounded-full cursor-pointer"
               onClick={() => {
                 setToggleDropdown(!toggleDropdown);
-                console.log('HELLO');
+                console.log("HELLO");
               }}
             />
+            <div>{user.email}</div>
+            <div className="logoutBtn border rounded border-red-600 flex justify-center items-center font-bold">
+              <button
+                onClick={handleLogout}
+                className="text-red-600 text-lg p-1 hover:bg-red-600 transition hover:text-white"
+              >
+                Logout
+              </button>
+            </div>
             {/* <button className="border rounded border-red-600 flex justify-center items-center p-1 gap-1 font-bold" onClick={signOutUser}>
               <AiOutlineLogout className="text-2xl text-red-600 " />
               <span className='text-red-600'>Log out</span>
@@ -81,7 +98,6 @@ const NavBar = () => {
             </a>
           </div>
         )}
-        {/* <SecondaryButton  SecondaryButtonText="Logout"/> */}
       </div>
     </div>
   );
