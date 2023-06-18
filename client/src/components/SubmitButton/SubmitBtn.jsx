0abb2c5b82989a5387b2bea9ab52759fe5c8c11f
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-18 00:59:46 +0000

    created a prop for disabled prop for  submit button component

--- client/src/components/SubmitButton/SubmitBtn.jsx
+++ client/src/components/SubmitButton/SubmitBtn.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const SubmitBtn = (props) => {
   return (
     <div className="border rounded transition  bg-black flex justify-center items-center font-bold hover:shadow-2xl hover:bg-green-600">
-      <button className="text-white text-lg p-1 ">{props.ButtonText}</button>
+      <button disabled={props.disabled} className="text-white text-lg p-1 disabled:bg-slate-300  ">{props.ButtonText}</button>
     </div>
   );
 };
