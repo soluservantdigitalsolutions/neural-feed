4c9b4808f478f12a33d4c4cf36b3e5e55cbbec12
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-19 05:41:52 +0000

    added post routes for login and logout

--- server/Routes/authRoute.js
+++ server/Routes/authRoute.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
-const { Register, Login } = require("../Controllers/AuthController.js");
+const { Register, Login, Logout } = require("../Controllers/AuthController.js");
 const User = require("../Models/UserModel.js");
-
+const requireAuth = require("../middleware/verifyToken.js");
 //Implementing auth Routes
 
+// router.use(requireAuth);
 
 router.post("/register", Register);
 
-router.post("/login", Login)
+router.post("/login", Login);
+
+router.post("/logout", Logout);
 
 module.exports = router;
