c444969e093c78ccf12f558b2ebe0c624835cc19
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-19 05:02:42 +0000

    AuthProvider has been removed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthenticationContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
