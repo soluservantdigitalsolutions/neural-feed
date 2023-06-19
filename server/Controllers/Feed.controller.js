4f703c42445a4ae53a0a452feb4d34e9660c78a2
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-19 05:24:39 +0000

    created feed controller

---
+++ server/Controllers/Feed.controller.js
@@ -0,0 +1,26 @@
+const UserModel = require("../Models/UserModel.js");
+const feedModel = require("../Models/feed.model.js");
+
+const postFeed = async (req, res) => {
+  //   const username = UserModel.findOne({ username });
+  try {
+    const newFeed = await feedModel.create({
+      author: UserModel.find(),
+      ...req.body,
+    });
+
+    //  await feedModel.bulkSave()
+    res.status(200).json({
+      feed: newFeed,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const getFeed = async (req, res) => {};
+
+module.exports = {
+  postFeed,
+  getFeed,
+};
