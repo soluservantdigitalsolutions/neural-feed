9d8c8213f4735a6382967843a11dda376ebe25cc
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-30 12:05:51 +0000

    moved db connection function to app.listen scope

--- server/server.js
+++ server/server.js
@@ -28,15 +28,15 @@ requestAndRequestPathLogger();
 app.use("/api/auth", authRoute);
 app.use("/api/upload", feedsRoute);
 
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => {
-    console.log("Database is connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 app.listen(process.env.PORT || 4000, () => {
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+      console.log("Database is connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });
