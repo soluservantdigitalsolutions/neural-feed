842c569d31295907fd8e004f59d6d34cd3616dfb
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-30 17:33:15 +0000

    updated server.js

--- server/server.js
+++ server/server.js
@@ -17,10 +17,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: [
-      "https://neural-feed.vercel.app/",
-      "https://neural-feed.vercel.app/register",
-    ],
+    origin: ["https://neural-feed.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
