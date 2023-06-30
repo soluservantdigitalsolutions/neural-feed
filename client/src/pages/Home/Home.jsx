2dc0152dee62290576db3d5c81b243896d9cb686
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-30 12:04:58 +0000

    added elements to the home button

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,40 +1,61 @@
-import React, { useState } from 'react';
-import UserProfileImage from '../../assets/user (1).png';
-import SecondaryButton from '../../components/SecondaryButton/SecondaryButton';
-import TestVideo from '../../assets/Are You Using This EQ Trick- Left and Right Channel EQ.mp4';
-import { BsFillPauseFill, BsFillPlayFill } from 'react-icons/bs';
-import {HiVolumeUp, HiVolumeOff} from "react-icons/hi"
-import { useRef } from 'react';
-import testVideo2 from '../../assets/Fl studio 11 trick- how to use your own headphone with mic to record you voice on fl studio 11.mp4';
+import React, { useEffect, useState } from "react";
+import UserProfileImage from "../../assets/user (1).png";
+import SecondaryButton from "../../components/SecondaryButton/SecondaryButton";
+import TestVideo from "../../assets/Are You Using This EQ Trick- Left and Right Channel EQ.mp4";
+import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { useRef } from "react";
+import testVideo2 from "../../assets/Fl studio 11 trick- how to use your own headphone with mic to record you voice on fl studio 11.mp4";
+import axios from "axios";
+import { FaGraduationCap } from "react-icons/fa";
+import { CiStreamOn } from "react-icons/ci";
+import { FaComments } from "react-icons/fa";
+import { FaHandsHelping } from "react-icons/fa";
 
 const Home = () => {
   const [isHover, setIsHover] = useState(false);
   const [videoIsPlaying, setvideoIsPlaying] = useState(false);
   const [isVideoMuted, setisVideoMuted] = useState(false);
+  const [video, setVideo] = useState();
+  const [username, setUsername] = useState("");
+  const [caption, setCaption] = useState("");
 
-  const videoRef = useRef(null)
-
-  const onVideoPress = () =>{
-    if (videoIsPlaying) {
-      videoRef?.current?.pause();
-      setvideoIsPlaying(false)
-    } else {
-      videoRef?.current?.play();
-      setvideoIsPlaying(true)
-    }
-  }
-  
-    const onMuteButtonPress = () => {
-      if (isVideoMuted) {
-        // videoRef?.current?.muted;
-        setisVideoMuted(false);
-      } else {
-        // !videoRef?.current?.muted;
-        setisVideoMuted(true);
+  useEffect(() => {
+    const getFeeds = async () => {
+      try {
+        const feeds = await axios.get("http://localhost:3000/api/upload/feeds");
+        console.log(feeds);
+        setVideo(feeds.data.feeds[0].video);
+        setUsername(feeds.data.feeds[0].username);
+        setCaption(feeds.data.feeds[0].caption);
+      } catch (err) {
+        console.log(err.message);
       }
     };
-  
-  
+    getFeeds();
+  }, []);
+
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (videoIsPlaying) {
+      videoRef?.current?.pause();
+      setvideoIsPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setvideoIsPlaying(true);
+    }
+  };
+
+  const onMuteButtonPress = () => {
+    if (isVideoMuted) {
+      // videoRef?.current?.muted;
+      setisVideoMuted(false);
+    } else {
+      // !videoRef?.current?.muted;
+      setisVideoMuted(true);
+    }
+  };
 
   return (
     <div className="flex flex-col gap-10 justify-center border p-5 ">
@@ -44,15 +65,16 @@ const Home = () => {
             <img
               src={UserProfileImage}
               alt=""
-              className="w-10"
+              className="w-10 transition "
+              // loading="true"
             />
           </div>
           <div className="UserNameAndDetailsDiv">
             <div className="UsernameDiv">
-              <h1 className="font-bold">testuser</h1>
+              <h1 className="font-bold">{username}</h1>
             </div>
             <div className="VideoCaptionDiv">
-              <p>This is a test Video Caption</p>
+              <p>{caption}</p>
             </div>
           </div>
           <div className="EnrollButtonDiv flex items-center justify-center">
@@ -68,16 +90,16 @@ const Home = () => {
           }}
           className="VideoFileDiv flex justify-center items-center rounded flex-col"
         >
-          <video
-            src={TestVideo}
-            className="w-full max-w-2xl rounded transition"
+          <img
+            src={video}
+            className="w-80 rounded transition max-w-lg"
             ref={videoRef}
-            onClick={onVideoPress}
+            // onClick={onVideoPress}
             loop
             // autoPlay
 
             // controls
-          ></video>
+          ></img>
           {isHover && (
             <div className="absolute text-white cursor-pointer transition">
               {videoIsPlaying ? (
@@ -101,10 +123,31 @@ const Home = () => {
             </div>
           )}
         </div>
-        <br />
-        <hr  className='text-black border w-48 border-black m-5'/>
+        <div className="  userReaction flex flex-col gap-1  text- top-52  w-47 p- justify-center items-center  ">
+          <div className=" statisticsDiv flex gap-20  text- top-52  w-47 p-5 justify-center items-center">
+            <div className="GradHats flex flex-col justify-center items-center">
+              <FaHandsHelping className="cursor-pointer text-5xl p-3 border rounded-full bg-slate-200" />
+              <span>1M</span>
+            </div>
+            <div className="GradHats flex flex-col justify-center items-center">
+              <FaComments className=" cursor-pointer text-5xl p-3 border rounded-full bg-slate-200" />
+              <span>457</span>
+            </div>
+            <div className="GradHats flex flex-col justify-center items-center">
+              <FaGraduationCap className=" cursor-pointer text-5xl p-3  rounded-full bg-slate-200 " />
+              <span>4.7k</span>
+            </div>
+          </div>
+          <div className="ViewsDiv ">
+            <div className="viewsNumber flex flex-col justify-center items-center">
+              <CiStreamOn className="text-3xl" />
+              <span>1,334,489</span>
+            </div>
+          </div>
+        </div>
+        {/* <br /> */}
+        <hr className="text-black border w-48 border-black m-5" />
       </div>
-      
     </div>
   );
 };
