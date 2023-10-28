8cdfed2656ae11a8d63f5db30ee870dc1d474e15
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-28 19:45:23 +0000

    Created and styled a Submit Button

--- client/src/components/SubmitButton/SubmitBtn.jsx
+++ client/src/components/SubmitButton/SubmitBtn.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SubmitBtn = (props) => {
   return (
-    <div className="border rounded transition  bg-black flex justify-center items-center font-bold hover:shadow-2xl hover:bg-green-600">
-      <button disabled={props.disabled} type="submit" className="text-white text-lg p-1 disabled:bg-slate-300  ">{props.ButtonText}</button>
+    <div className="border rounded transition  bg-green-600 flex justify-center items-center font-bold hover:shadow-2xl hover:bg-green-700">
+      <button
+        disabled={props.disabled}
+        type="submit"
+        className="text-white text-lg p-1 disabled:bg-slate-300  "
+        onClick={props.onClick}
+      >
+        {props.ButtonText}
+      </button>
     </div>
   );
 };
