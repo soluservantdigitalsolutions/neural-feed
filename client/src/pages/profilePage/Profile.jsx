a33476a8b1101559a5a56590e904345e3bf8f835
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-28 21:32:03 +0000

    I replaces the localStorage CurrentUser state with dispatches

--- client/src/pages/profilePage/Profile.jsx
+++ client/src/pages/profilePage/Profile.jsx
@@ -1,41 +1,49 @@
-import React from 'react';
-import testProfilePic from "../../assets/user (1).png"
+import React from "react";
+import testProfilePic from "../../assets/user (1).png";
+import SecondaryButton from "../../components/SecondaryButton/SecondaryButton";
+import { useSelector } from "react-redux/es";
 
 const Profile = () => {
   // const [user] = useAuthState(Auth);
+
+  const { currentUser } = useSelector((state) => state.user);
+  const userData = currentUser.user;
+  console.log("UserData", userData);
 
   return (
     <div className="flex flex-col ">
       <div className="ProfileInfoAndStats flex flex-col justify-center items-center p-5 gap-5 border w-full">
         <div className="ProfilePicDiv ">
           <img
-             src={testProfilePic}
+            src={userData.profileImage ? userData.profileImage : testProfilePic}
             alt="testUser"
-            className="rounded-full w-20"
+            className="rounded-full w-[100px] h-[100px] object-cover"
           />
         </div>
         <div className="UserNameDiv">
-          <h1 className="font-bold">This User</h1>
+          <h1 className="font-bold">{userData.username}</h1>
         </div>
         <div className="userStatsDiv flex gap-5">
           <div className="Admissions">
-            <span className="font-bold">50</span>
-            <span className="font-thin"> Admissions</span>
+            <span className="font-bold">{userData.enrollments.length}</span>
+            <span className="font-thin"> Enrollments</span>
           </div>
           <div className="StudentsDiv">
-            <span className="font-bold">1,000</span>
-            <span> Students</span>
+            <span className="font-bold">{userData.admissions.length}</span>
+            <span> Admissions</span>
           </div>
           <div className="GraduationHatsDiv">
-            <span className="font-bold">4000</span>
+            <span className="font-bold">{userData.hats.length}</span>
             <span> Hats</span>
           </div>
         </div>
+        {/* <div className="BioDIV">
+          <SecondaryButton SecondaryButtonText="Edit Profile" />
+        </div> */}
         <div className="BioDIV">
-          <p>Test Bio</p>
+          <p>{userData.bio ? userData.bio : ""}</p>
         </div>
       </div>
-      
     </div>
   );
 };
