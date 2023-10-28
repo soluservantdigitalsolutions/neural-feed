33eee5ab07407cd64ed84b89ae043c0e8988758b
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-28 19:42:24 +0000

    added a Onchange prop to receive files

--- client/src/components/DropZone/Dropzone.jsx
+++ client/src/components/DropZone/Dropzone.jsx
@@ -1,25 +1,22 @@
-import React, { useState } from "react";
 import { RiVideoUploadFill } from "react-icons/ri";
-import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+
 import "./DropZone.css";
 
-const Dropzone = (props) => {
-
-
+const Dropzone = ({onChange}) => {
   return (
-    <div className="UploadCardSpaceDiv w-full h-full border-dashed border-green-600 bg-slate-100 cursor-pointer hover:bg-green-100 transition flex flex-col justify-center text-center p-5 border ">
-      <label htmlFor="file">
-        <div className="UploadIconDiv flex flex-col justify-center items-center text-center w-full h-full">
+    <div className="UploadCardSpaceDiv w-full h-full border-dashed border-green-600 bg-slate-100 cursor-pointer  hover:bg-green-100 transition flex flex-col justify-center text-center p-5 border ">
+      <label
+        htmlFor="file"
+        className=""
+      >
+        <div className="UploadIconDiv flex flex-col justify-center items-center text-center w-full h-full ">
           <RiVideoUploadFill className="text-5xl text-center " />
           <h1>Click to Upload</h1>
-          <p 
-              >{props.Filename }</p>
         </div>
         {/* <div className="InstructionalDiv">
         <p>Or Drag and Drop your Video File</p>
       </div> */}
-        <div className="flex justify-center hidden">
+        <div className=" justify-center hidden">
           <div className="UploadButtonDiv flex justify-center items-center">
             <input
               type="file"
@@ -32,7 +29,7 @@ const Dropzone = (props) => {
                 justifyContent: "center",
                 alignItems: "center",
               }}
-              onChange={props.onChange}
+              onChange={onChange}
             />
           </div>
         </div>
