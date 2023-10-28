2c8b633e86dd139a2e7f59283aa6dfa026a0a17f
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-28 19:20:48 +0000

    created and added my redux provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate
+          loading={null}
+          persistor={persistor}
+        >
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
