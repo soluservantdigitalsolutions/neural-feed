58081972c0cadc3dc31fd3bb126e95a5ba71d236
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-29 08:34:15 +0000

    update

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: " http://localhost:5173/",
+    origin: " http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
