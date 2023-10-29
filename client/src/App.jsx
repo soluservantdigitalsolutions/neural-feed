30452713971fea0aa468f22b978ad98487185656
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-29 03:19:03 +0000

    added back protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Login from "./pages/LoginPage/Login";
 import SignUp from "./pages/SignUpPage/SignUp";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/profilePage/Profile";
 import NavBar from "./components/NavBar/NavBar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Auth } from "../firebase.config";
 import Upload from "./pages/UploadPage/Upload";
+import { useSelector } from "react-redux/es";
 
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
   const location = useLocation();
 
   return (
@@ -30,17 +34,17 @@ function App() {
         <Route
           exact
           path="/"
-          element={<Home />}
+          element={currentUser ? <Home /> : <Navigate to="/login" />}
         />
         <Route
           exact
           path="/profile/:id"
-          element={<Profile />}
+          element={currentUser ? <Profile /> : <Navigate to="/login" />}
         />
         <Route
           exact
           path="/upload"
-          element={<Upload />}
+          element={currentUser ? <Upload /> : <Navigate to="/login" />}
         />
       </Routes>
     </>
