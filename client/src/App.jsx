536b4dc2654410d075c0dba574f0b52f0d2e1565
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-29 02:53:12 +0000

    updated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
 import "./App.css";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./pages/LoginPage/Login";
 import SignUp from "./pages/SignUpPage/SignUp";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/profilePage/Profile";
 import NavBar from "./components/NavBar/NavBar";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Auth } from "../firebase.config";
 import Upload from "./pages/UploadPage/Upload";
-import { useSelector } from "react-redux/es";
 
 function App() {
-  const { currentUser } = useSelector((state) => state.user);
   const location = useLocation();
 
   return (
@@ -34,17 +30,17 @@ function App() {
         <Route
           exact
           path="/"
-          element={currentUser ? <Home /> : <Navigate to="/login" />}
+          element={<Home />}
         />
         <Route
           exact
           path="/profile/:id"
-          element={currentUser ? <Profile /> : <Navigate to="/login" />}
+          element={<Profile />}
         />
         <Route
           exact
           path="/upload"
-          element={currentUser ? <Upload /> : <Navigate to="/login" />}
+          element={<Upload />}
         />
       </Routes>
     </>
