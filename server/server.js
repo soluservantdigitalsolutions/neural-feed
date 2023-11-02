9fec3439dd1c0ec80d974171ca95683d7b44a818
asantesylvesterkwame <intelligentson17@gmail.com>
2023-11-02 17:27:15 +0000

    changed origin to production times

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: "https://neural-feed.onrender.com",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
