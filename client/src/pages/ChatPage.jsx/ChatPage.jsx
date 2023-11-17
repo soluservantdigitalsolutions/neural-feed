3fc47a61970f3d09769ad62f37488c76d2535104
asantesylvesterkwame <intelligentson17@gmail.com>
2023-11-17 17:33:28 +0000

    created the ChatPage

---
+++ client/src/pages/ChatPage.jsx/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBox from "../../components/ChatBox/ChatBox";
+
+const ChatPage = () => {
+  return (
+    <div className="h-screen bg-gray-100 overflow-hidden">
+      <ChatBox />
+    </div>
+  );
+};
+
+export default ChatPage;
