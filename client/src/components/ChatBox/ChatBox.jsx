85bb919b0553f702eed231bd6419a92f6583fc80
asantesylvesterkwame <intelligentson17@gmail.com>
2023-11-17 17:35:27 +0000

    Imported Generate Response

--- client/src/components/ChatBox/ChatBox.jsx
+++ client/src/components/ChatBox/ChatBox.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { generateResponse } from "../../utils/OpenAiService";
+import { generateResponse } from "../../utils/AiService";
 import ReactMarkdown from "react-markdown";
 
 const ChatBox = () => {
