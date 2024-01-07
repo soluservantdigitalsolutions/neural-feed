4126cdb49ab40471004a6871b8cfd5888d46dcb4
asantesylvesterkwame <intelligentson17@gmail.com>
2024-01-07 18:57:37 +0000

    added a Note Context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react.js";
+import { NoteProvider } from "./context/noteContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
+    <NoteProvider>
+      <BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </NoteProvider>
   </Provider>
 );
