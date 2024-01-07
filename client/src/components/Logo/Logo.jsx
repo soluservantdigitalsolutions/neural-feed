8b2fe7c26b03eb5a50d10efaa570a5f72aae8304
asantesylvesterkwame <intelligentson17@gmail.com>
2024-01-07 18:58:44 +0000

    linted

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div>
       <a href="/">
-        <h1 className="font-bold text-2xl cursor-pointer ">
+        <h1 className=" font-bold text-2xl cursor-pointer ">
           <span className="text-green-600">Neural</span> Feed
         </h1>
       </a>
