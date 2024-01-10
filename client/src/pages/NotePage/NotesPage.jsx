02e894ade1f50abc5a49c331cf0e83c36f3b318e
asantesylvesterkwame <intelligentson17@gmail.com>
2024-01-10 12:21:59 +0000

    added and used the skeleton loader

--- client/src/pages/NotePage/NotesPage.jsx
+++ client/src/pages/NotePage/NotesPage.jsx
@@ -1,17 +1,17 @@
 import React, { useContext } from "react";
 import { NoteContext } from "../../context/noteContext";
 import NotePreview from "../../components/NotesPreview/NotePreview";
+import SkeletonLoader from "../../components/SkeletonLoader/SkeletonLoader";
 
 const NotesPage = () => {
-  const { notes } = useContext(NoteContext);
+  const { notes, loading } = useContext(NoteContext);
 
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {notes?.map((note) => (
-        <NotePreview key={note._id} note={note} />
-      ))}
+      {loading
+        ? Array.from({ length: 10 }).map((_, i) => <SkeletonLoader key={i} />)
+        : notes?.map((note) => <NotePreview key={note._id} note={note} />)}
     </div>
   );
 };
-
 export default NotesPage;
