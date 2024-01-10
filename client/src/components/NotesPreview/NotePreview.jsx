8d24fb4611ee7efc2b8b005a07ed02b69a7c578f
asantesylvesterkwame <intelligentson17@gmail.com>
2024-01-10 12:20:37 +0000

    removed the shorten article content

--- client/src/components/NotesPreview/NotePreview.jsx
+++ client/src/components/NotesPreview/NotePreview.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NotePreview = ({ note }) => {
+  const [showFullContent, setShowFullContent] = useState(false);
+  const maxLength = 900; // Adjust this number to control how much content is shown initially
+
+  let contentToDisplay = note.content;
+  if (!showFullContent && note.content.length > maxLength) {
+    const lastSpaceIndex = note.content.lastIndexOf(" ", maxLength);
+    contentToDisplay = note.content.slice(0, lastSpaceIndex) + "...";
+    console.log(contentToDisplay);
+  }
+
   return (
     <div className="p-4 border rounded shadow">
       <img
@@ -9,7 +19,16 @@ const NotePreview = ({ note }) => {
         className="w-full h-64 object-cover mb-4 rounded"
       />
       <h2 className="text-xl font-bold mb-2">{note.title}</h2>
-      <p className="text-gray-700">{note.content}</p>
+      {/* <p dangerouslySetInnerHTML={{ __html: contentToDisplay }}></p> */}
+
+      {note.content.length > maxLength && !showFullContent && (
+        <button
+          className="border bg-green-600 p-2 text-white rounded font-bold"
+          onClick={() => setShowFullContent(true)}
+        >
+          Feed More <span className="font-extrabold">. . .</span>
+        </button>
+      )}
     </div>
   );
 };
