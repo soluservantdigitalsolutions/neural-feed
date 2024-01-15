7b3f84eb49bf4a192faa816f631ed1a6ef2680aa
asantesylvesterkwame <intelligentson17@gmail.com>
2024-01-15 03:51:06 +0000

    updated

--- client/src/pages/LoginPage/Login.jsx
+++ client/src/pages/LoginPage/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
     dispatch(loginStart());
     try {
       const res = await axios.post(
-        "https://neural-feed-backend-2yg8.onrender.com//api/auth/login",
+        "https://neural-feed-backend-2yg8.onrender.com/api/auth/login",
         {
           username,
           password,
