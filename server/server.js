1def508c6d33b7b4416c10b48ab4dde6e65bdcc9
asantesylvesterkwame <intelligentson17@gmail.com>
2024-02-04 10:29:27 +0000

    changed the origin

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://neuralfeed.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
